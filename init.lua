f8dab6303988e9052e6faaf2a535411bd0f991bb
刘军 <liujun@gw.com.cn>
2020-12-18 18:36:28 +0800

    vimL => lua

--- init.lua
+++ init.lua
@@ -9,12 +9,6 @@ vim.cmd('let mapleader=" "')
 --     e
 --     v
 
--- vim.api.nvim_set_keymap('n', '<leader><Space>', ':set hlsearch<CR>', { noremap = true, silent = true })
--- :nnoremap <silent> <leader><Space> :set hlsearch<CR>
-
--- vim.api.nvim_buf_set_keymap(0, '', 'cc', 'line(".") == 1 ? "cc" : "ggcc"', { noremap = true, expr = true })
--- :noremap <buffer> <expr> cc line('.') == 1 ? 'cc' : 'ggcc'
-
 -- vim.cmd('noremap n j')
 vim.api.nvim_set_keymap('', 'n', 'j', { noremap=true, silent=true })
 -- vim.cmd('noremap N J')
@@ -82,6 +76,18 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function()
   -- Packer can manage itself as an optional plugin
     use {'wbthomason/packer.nvim', opt = true}
+    use 'norcalli/nvim-colorizer.lua'
+    use 'cespare/vim-toml'
+    use 'euclidianAce/BetterLua.vim'
+    use 'yggdroot/indentLine'
+    use { 'thinca/vim-quickrun' }
+    vim.g.quickrun_no_default_key_mappings = 1
+    vim.api.nvim_set_keymap('n', '<leader>rr', '<Plug>(quickrun)', { noremap=false, silent=false })
+    vim.api.nvim_exec(
+      [[
+        let g:quickrun_config = { "_" : { "outputter" : "message" }}
+      ]],
+    false)
 
     use {
         'glepnir/galaxyline.nvim',
@@ -99,9 +105,10 @@ return require('packer').startup(function()
     vim.cmd("set background=dark")
 
     use {
-        'luochen1990/rainbow'
+        'luochen1990/rainbow',
     }
-    vim.cmd("let g:rainbow_active = 1")
+    -- vim.cmd("let g:rainbow_active = 1")
+    vim.g.rainbow_active = 1
 
     use {
         'jiangmiao/auto-pairs',
@@ -113,58 +120,75 @@ return require('packer').startup(function()
     use { 'kana/vim-textobj-user' }
 
     use { 'kana/vim-textobj-indent', requires = {'kana/vim-textobj-user'} }
-    vim.cmd(
-      [[
-        let g:textobj_indent_no_default_key_mappings=1
+    vim.g.textobj_indent_no_default_key_mappings = 1
 
-        xmap uu <Plug>(textobj-indent-i)
-        omap uu <Plug>(textobj-indent-i)
-        xmap uU <Plug>(textobj-indent-same-i)
-        omap uU <Plug>(textobj-indent-same-i)
+    vim.api.nvim_set_keymap('x', 'uu', '<Plug>(textobj-indent-i)', {})
+    vim.api.nvim_set_keymap('o', 'uu', '<Plug>(textobj-indent-i)', {})
 
-        xmap au <Plug>(textobj-indent-a)
-        omap au <Plug>(textobj-indent-a)
-        xmap aU <Plug>(textobj-indent-same-a)
-        omap aU <Plug>(textobj-indent-same-a)
-      ]]
-    )
+    vim.api.nvim_set_keymap('x', 'uU', '<Plug>(textobj-same-i)', {})
+    vim.api.nvim_set_keymap('o', 'uU', '<Plug>(textobj-same-i)', {})
+
+    vim.api.nvim_set_keymap('x', 'au', '<Plug>(textobj-indent-a)', {})
+    vim.api.nvim_set_keymap('o', 'au', '<Plug>(textobj-indent-a)', {})
+
+    vim.api.nvim_set_keymap('x', 'aU', '<Plug>(textobj-same-a)', {})
+    vim.api.nvim_set_keymap('o', 'aU', '<Plug>(textobj-same-a)', {})
 
     use {
       'sgur/vim-textobj-parameter',
       requires = { 'kana/vim-textobj-user' }
     }
-    vim.cmd(
-      [[
-        let g:textobj_parameter_no_default_key_mappings=1
+    vim.g.textobj_parameter_no_default_key_mappings = 1
 
-        xmap ua <Plug>(textobj-parameter-i)
-        omap ua <Plug>(textobj-parameter-i)
-        xmap aa <Plug>(textobj-parameter-a)
-        omap aa <Plug>(textobj-parameter-a)
-      ]]
-    )
+    vim.api.nvim_set_keymap('x', 'u,', '<Plug>(textobj-parameter-i)', {})
+    vim.api.nvim_set_keymap('o', 'u,', '<Plug>(textobj-parameter-i)', {})
+
+    vim.api.nvim_set_keymap('x', 'a,', '<Plug>(textobj-parameter-a)', {})
+    vim.api.nvim_set_keymap('o', 'a,', '<Plug>(textobj-parameter-a)', {})
 
     use {
       'kdheepak/lazygit.nvim',
       cmd = { 'LazyGit' },
     }
-    vim.cmd(
-      [[
-        nnoremap <silent> <leader>gg :LazyGit<CR>
-        let g:lazygit_floating_window_winblend = 0
-        let g:lazygit_floating_window_scaling_factor = 0.9
-      ]]
-    )
+    vim.api.nvim_set_keymap('n', '<leader>gg', ':LazyGit<CR>', { noremap=true, silent=true })
+    vim.g.lazygit_floating_window_winblend = 0
+    vim.g.lazygit_floating_window_scaling_factor = 0.9
 
     use {
       'kyazdani42/nvim-tree.lua'
     }
-    vim.cmd("nnoremap tt :LuaTreeToggle<CR>")
+    vim.api.nvim_set_keymap('n', 'tt', ':LuaTreeToggle<CR>', { noremap=true, silent=true })
+    
+    use {
+      'tpope/vim-surround',
+      event = { 'BufReadPre *', 'BufNewFile *'}
+    }
+    
+    use {
+      'Yggdroot/LeaderF',
+      cmd = { '<leader>ff' },
+      run = './install.sh'
+    }
+    vim.api.nvim_exec(
+      [[
+        let g:Lf_ShortcutF = "<leader>ff"
+        let g:Lf_CommandMap = {'<C-K>': ['<C-e>'], '<C-J>': ['<C-n>']}
+
+        let g:Lf_WindowPosition = 'popup'
+        let g:Lf_PreviewInPopup = 1
+        let g:Lf_StlSeparator = { 'left': "\ue0b0", 'right': "\ue0b2" }
+        let g:Lf_PreviewResult = {'Function': 0, 'BufTag': 0 }
+
+        noremap <leader>fb :<C-U><C-R>=printf("Leaderf buffer %s", "")<CR><CR>
+        noremap <leader>fm :<C-U><C-R>=printf("Leaderf mru %s", "")<CR><CR>
+        noremap <leader>fl :<C-U><C-R>=printf("Leaderf line %s", "")<CR><CR>
+        noremap <leader>fr :<C-U><C-R>=printf("Leaderf rg %s", "")<CR><CR>
+      ]],
+    false)
 
     use {
       'neoclide/coc.nvim'
     }
-
     vim.api.nvim_exec(
       [[
         let g:coc_snippet_next = '<TAB>'
