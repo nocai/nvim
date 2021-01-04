0433c6ef8aabc0d881298286e288221930de2e39
刘军 <liujun@gw.com.cn>
2021-01-04 10:17:42 +0800

    update lua/mappings.lua

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,39 +10,30 @@ function mappings.setup()
   --     e
   --     v
 
-  -- vim.cmd('noremap n j')
+  -- nN => jJ => eE => kK => nN
   api.nvim_set_keymap('', 'n', 'j', { noremap=true, silent=true })
-  -- vim.cmd('noremap N J')
   api.nvim_set_keymap('', 'N', 'J', { noremap=true, silent=true })
   -- vim.cmd('noremap <c-n> 5j')
   api.nvim_set_keymap('', '<c-n>', '5j', { noremap=true, silent=true })
-  -- vim.cmd('noremap <c-w>n <c-w>j')
   api.nvim_set_keymap('', '<c-w>n', '<c-w>j', { noremap=true, silent=true })
 
-  -- vim.cmd('noremap j e')
   api.nvim_set_keymap('', 'j', 'e', { noremap=true, silent=true })
-  -- vim.cmd('noremap J E')
   api.nvim_set_keymap('', 'J', 'E', { noremap=true, silent=true })
   -- vim.cmd('noremap <c-j> <c-e>')
   api.nvim_set_keymap('', '<c-j>', '<c-e>', { noremap=true, silent=true })
 
-  -- vim.cmd('noremap e k')
   api.nvim_set_keymap('', 'e', 'k', { noremap=true, silent=true })
-  -- vim.cmd('noremap E K')
   api.nvim_set_keymap('', 'E', 'K', { noremap=true, silent=true })
   -- vim.cmd('noremap <c-e> 5k')
   api.nvim_set_keymap('', '<c-e>', '5k', { noremap=true, silent=true })
   -- vim.cmd('noremap <c-w>e <c-w>k')
   api.nvim_set_keymap('', '<c-w>e', '<c-w>k', { noremap=true, silent=true })
 
-  -- vim.cmd('noremap k n')
   api.nvim_set_keymap('', 'k', 'n', { noremap=true, silent=true })
-  -- vim.cmd('noremap K N')
   api.nvim_set_keymap('', 'K', 'N', { noremap=true, silent=true })
 
-  -- vim.cmd('noremap i l')
+  -- iI => lL => iI
   api.nvim_set_keymap('', 'i', 'l', { noremap=true, silent=true })
-  -- vim.cmd('noremap I L')
   api.nvim_set_keymap('', 'I', 'L', { noremap=true, silent=true })
   -- vim.cmd('noremap <c-w>i <c-w>l')
   api.nvim_set_keymap('', '<c-w>i', '<c-w>l', { noremap=true, silent=true })
