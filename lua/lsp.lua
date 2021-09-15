30aa7d04dbf90dc15a9c1180bebbb5e42330e459
刘军 <liujun@gw.com.cn>
2021-09-15 17:38:40 +0800

    lsp.lua

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -35,7 +35,7 @@ local on_attach = function(client, bufnr)
   -- Mappings.
   local opts = {noremap = true, silent = true}
   buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  -- buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
   buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
   buf_set_keymap("n", "gy", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
   buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
@@ -45,7 +45,7 @@ local on_attach = function(client, bufnr)
   buf_set_keymap("n", "ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 
   buf_set_keymap("n", "E", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  buf_set_keymap("i", "<C-e>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  buf_set_keymap("i", "<C-h>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
 
   buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
   buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
