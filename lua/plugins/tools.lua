52671919bb7be2b1fffe519daa3ba1cc7657252c
刘军 <liujun@gw.com.cn>
2021-12-27 19:17:06 +0800

    add or remove plugins

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -51,23 +51,6 @@ local function textobject_parameter()
 		]])
 end
 
-local function formatter()
-	require("formatter").setup({
-		filetype = {
-			-- luafmt
-			lua = {
-				function()
-					return {
-						exe = "luafmt",
-						args = { "--indent-count", 2, "--stdin" },
-						stdin = true,
-					}
-				end,
-			},
-		},
-	})
-end
-
 local function autopairs()
 	require("nvim-autopairs").setup({})
 
@@ -76,6 +59,27 @@ local function autopairs()
 		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
 	end
+end
+
+local function tabout()
+	require("tabout").setup({
+		tabkey = "<Tab>", -- key to trigger tabout, set to an empty string to disable
+		backwards_tabkey = "<S-Tab>", -- key to trigger backwards tabout, set to an empty string to disable
+		act_as_tab = true, -- shift content if tab out is not possible
+		act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+		enable_backwards = true, -- well ...
+		completion = true, -- if the tabkey is used in a completion pum
+		tabouts = {
+			{ open = "'", close = "'" },
+			{ open = '"', close = '"' },
+			{ open = "`", close = "`" },
+			{ open = "(", close = ")" },
+			{ open = "[", close = "]" },
+			{ open = "{", close = "}" },
+		},
+		ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+		exclude = {}, -- tabout will ignore these filetypes
+	})
 end
 
 -- git
@@ -113,11 +117,6 @@ return {
 		end,
 	},
 	{
-		"mhartington/formatter.nvim",
-		disable = vim.nv.is_vscode,
-		config = formatter,
-	},
-	{
 		"terrortylor/nvim-comment",
 		disable = vim.nv.is_vscode,
 		event = "BufReadPre",
@@ -127,17 +126,18 @@ return {
 	},
 	-- textobject
 	{
-		"kana/vim-textobj-user",
-		event = { "BufRead" },
-	},
-	{
-		"kana/vim-textobj-indent",
-		after = { "vim-textobj-user" },
-		setup = function()
-			vim.g.textobj_indent_no_default_key_mappings = 1
-		end,
-		config = function()
-			vim.cmd([[
+		{
+			"kana/vim-textobj-user",
+			event = { "BufRead" },
+		},
+		{
+			"kana/vim-textobj-indent",
+			after = { "vim-textobj-user" },
+			setup = function()
+				vim.g.textobj_indent_no_default_key_mappings = 1
+			end,
+			config = function()
+				vim.cmd([[
 				xmap ll <Plug>(textobj-indent-i)
 				omap ll <Plug>(textobj-indent-i)
 				xmap lL <Plug>(textobj-indent-same-i)
@@ -148,19 +148,26 @@ return {
 				xmap aL <Plug>(textobj-indent-same-a)
 				omap aL <Plug>(textobj-indent-same-a)
 			]])
-		end,
-	},
-	{
-		"sgur/vim-textobj-parameter",
-		after = { "vim-textobj-user" },
-		setup = function()
-			vim.g.textobj_parameter_no_default_key_mappings = 1
-		end,
-		config = textobject_parameter,
+			end,
+		},
+		{
+			"sgur/vim-textobj-parameter",
+			after = { "vim-textobj-user" },
+			setup = function()
+				vim.g.textobj_parameter_no_default_key_mappings = 1
+			end,
+			config = textobject_parameter,
+		},
 	},
 	{
 		"windwp/nvim-autopairs",
 		after = "nvim-cmp",
 		config = autopairs,
+	},
+	{
+		"abecodes/tabout.nvim",
+		config = tabout,
+		wants = { "nvim-treesitter" }, -- or require if not used so far
+		after = "nvim-cmp",
 	},
 }
